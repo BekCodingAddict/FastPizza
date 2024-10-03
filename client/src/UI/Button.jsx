93ed78fccb1a2661d510519e70b6a263c400a356
unknown <experte001@gmail.com>
2024-10-03 22:26:16 +0900

    Styling the Cart has been done!

--- client/src/UI/Button.jsx
+++ client/src/UI/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "px-4 py-3 md:px-6 md:py-3.5 inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wider text-stone-800 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-yellow-300 focus:ring focus:ring-stone-200 focus:ring-offset-1 disabled:cursor-not-allowed ",
   };
   if (to)
     return (
