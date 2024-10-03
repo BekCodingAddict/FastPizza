444977e410490f55e5a8011bad83e477a67c1cf8
unknown <experte001@gmail.com>
2024-10-03 18:36:17 +0900

    FlexBox modelling has been done!

--- client/src/UI/Header.jsx
+++ client/src/UI/Header.jsx
@@ -4,7 +4,7 @@ import UserName from "../features/user/UserName";
 
 function Header() {
   return (
-    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
       <Link className="uppercase tracking-widest" to="/">
         Fast React Pizza Co.
       </Link>
