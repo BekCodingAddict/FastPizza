49a8bbb6297242bd885d3485c1d498e88d8b0cc3
unknown <experte001@gmail.com>
2024-10-04 20:53:48 +0900

    Updating Cart Quantities has been done!

--- client/src/UI/Button.jsx
+++ client/src/UI/Button.jsx
@@ -9,6 +9,7 @@ function Button({ children, disabled, to, type, onClick }) {
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
     secondary:
       "px-4 py-3 text-sm md:px-6 md:py-3.5 inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wider text-stone-800 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-yellow-300 focus:ring focus:ring-stone-200 focus:ring-offset-1 disabled:cursor-not-allowed ",
+    round: base + "px-2.5 py-1  md:px-3.5 md:py-2 text-sm",
   };
 
   if (onClick)
