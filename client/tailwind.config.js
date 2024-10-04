354d9090ea7e4ce5b2c9e29cf0188dfab6eacdee
unknown <experte001@gmail.com>
2024-10-05 02:57:47 +0900

    Updaring Data without page Navigation practices

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,6 +9,10 @@ export default {
       height: {
         screen: "100dvh",
       },
+      backgroundImage: {
+        pizzas:
+          "url(https://c4.wallpaperflare.com/wallpaper/234/543/684/food-pizza-wallpaper-preview.jpg)",
+      },
     },
   },
   plugins: [],
