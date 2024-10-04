5ab384cdf05bb8f633414041e8e57ee2fcb266b7
unknown <experte001@gmail.com>
2024-10-04 18:26:32 +0900

    Adding new Item to cart has been done!

--- client/src/UI/Button.jsx
+++ client/src/UI/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const base =
     "inline-block text-sm rounded-full bg-yellow-400  font-semibold uppercase tracking-wider text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed ";
 
@@ -10,12 +10,21 @@ function Button({ children, disabled, to, type }) {
     secondary:
       "px-4 py-3 text-sm md:px-6 md:py-3.5 inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wider text-stone-800 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-yellow-300 focus:ring focus:ring-stone-200 focus:ring-offset-1 disabled:cursor-not-allowed ",
   };
+
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+    );
+
   if (to)
     return (
       <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
+
   return (
     <button disabled={disabled} className={styles[type]}>
       {children}
--- client/src/features/menu/MenuItem.jsx
+++ client/src/features/menu/MenuItem.jsx
@@ -1,8 +1,22 @@
 import Button from "../../UI/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  const handleAddToCart = (e) => {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  };
 
   return (
     <li className="flex gap-4 py-2">
@@ -25,7 +39,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add+</Button>
+          {!soldOut && (
+            <Button onClick={handleAddToCart} type="small">
+              Add+
+            </Button>
+          )}
         </div>
       </div>
     </li>
